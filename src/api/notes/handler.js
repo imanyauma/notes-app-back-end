f0dc099154fc7c498adf62dfabbdb7cdb3e1ccc3
Yauma Dzikri Imany <yauma8610@gmail.com>
2021-06-25 13:47:17 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -59,7 +59,7 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
+  async getNotesHandler(request) {
     const { id: credentialId } = request.auth.credentials;
     const notes = await this._service.getNotes(credentialId);
     return {
@@ -75,7 +75,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -117,7 +117,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       await this._service.editNoteById(id, request.payload);
 
       return {
